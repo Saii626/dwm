fcbdb7d99f000b781f916159fe1ccb9bdfb38e1b
saii626 <saikat626@gmail.com>
2020-07-06 01:38:32 +0530

    Don't quit

--- config.def.h
+++ config.def.h
@@ -99,7 +99,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_c,      recompile,      {0} },
 };
